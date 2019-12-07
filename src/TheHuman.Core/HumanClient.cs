b13adf54d0c0efaf82db8b7dcf4f039b6e6dd687
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-12-07 18:12:00 +0100

    Add Core & ConsoleApp projects

---
+++ src/TheHuman.Core/HumanClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheHuman.Core
+{
+    public class HumanClient
+    {
+        public void ConnectToDiscord(string username, string password)
+        {
+
+        }
+    }
+}

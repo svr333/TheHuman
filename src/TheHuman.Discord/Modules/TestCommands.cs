339649e9ef32a29cd1321bd3d49c2c3255d3722c
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2019-12-08 20:35:09 +0100

    Add test module

--- src/TheHuman.Discord/Modules/TestCommands.cs
+++ src/TheHuman.Discord/Modules/TestCommands.cs
@@ -1,7 +1,15 @@
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
 namespace TheHuman.Discord.Modules
 {
     public class TestCommands
     {
-        
+        [Command("ping")]
+        public async Task TestCommand(CommandContext context)
+        {
+            await context.RespondAsync("pong!");
+        }
     }
 }
